625a7258a4daa663ee08ef25eb211cd678b1f918
Crowley <jcrowley317@gmail.com>
2020-04-09 09:06:12 -0700

    puma edit
    josh's first commit

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,7 @@
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+
+# had to comment worker out to fix error
+#workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
