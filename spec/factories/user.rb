3d41fa3df85418d453217e233a3aa2165632b7de
Crowley <jcrowley317@gmail.com>
2020-04-30 09:50:31 -0700

    user.rb factory update

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,21 @@ FactoryBot.define do
 
   factory :user do
     email
+    pasword {"fake password"}
+    trait :admin do
+      role { 'admin' }
+    end
+
+/*    trait :organization_approved do
+      role { 'organization' }
+      organization
+    end
+*/
+
+    after(:create) do |user|
+      user.confirm
+    end
+
   end
 
 end
