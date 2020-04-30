6ae99d310f009fcb81b4af47c55235c361148932
Kyle Hannon <khannon@cocc.edu>
2020-04-30 08:58:57 -0700

    Working recaptcha

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+# let(:org) {Organization.new(name: "FAKE", email: "dataCollection@gmail.com")}
 
+# describe "relationship" do
+    #it "has many users" do
+        # expect(org).to
+
+
+
+# describe "#to_s" do
+    # it " has a string representation that is a name" do
+        # expect_name = "FAKE"
+        #expect(org).to eq(expexted_name)
+# end
 end
