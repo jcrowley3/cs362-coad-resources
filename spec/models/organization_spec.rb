d95eba9d6cb303adf395fe7f18c9023e0c72498b
Kyle Hannon <khannon@cocc.edu>
2020-05-05 09:00:36 -0700

    01 organization spec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-# let(:org) {Organization.new(name: "FAKE", email: "dataCollection@gmail.com")}
+let(:org) {Organization.new(name: "FAKE", email: "dataCollection@gmail.com")}
 
-# describe "relationship" do
-    #it "has many users" do
-        # expect(org).to
+#describe "relationship" do
+ #   it "has many users" do
+ #        expect(org).to eq(True)
+ #   end
+#end
 
 
-
-# describe "#to_s" do
-    # it " has a string representation that is a name" do
-        # expect_name = "FAKE"
-        #expect(org).to eq(expexted_name)
+ #describe "#to_s" do
+ #    it " has a string representation that is a name" do
+ #        expect_name = "FAKE"
+ #       expect(org).to eq(expexted_name)
 # end
 end
--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+    describe "to_s" do
+        it "returns valid name" do
+            expected_name = 'FAKE'
+            expect(expected_name.to_s).to eq.('FAKE')
 end
