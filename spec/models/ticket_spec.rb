248793032e201f88a128e656f764ad07728041fc
Crowley <jcrowley317@gmail.com>
2020-05-05 09:23:54 -0700

    ticket_spec.rb updated

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+
+#  let(:closed_ticket) { build(:closed_ticket) }
+#  let(:open_ticket) { build(:open_ticket) }
+
+#  describe 'relationships' do
+#    it { should belong_to(:region) }
+#  end
+
+#  describe "#open"
+#    it "retrieves only non_closed tickets without an organization" do
+#      open_tickets = Tickets.open
+#      expect(open_tickets).to include(open_ticket)
+#      expecct(open_tickets).not_to include(closed_ticket)
+#    end
+#  end
+
 end
