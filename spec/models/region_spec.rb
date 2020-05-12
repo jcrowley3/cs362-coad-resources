ccdfe47b4c46fbaa61e29df3f56b3cb1a40a9b97
Kyle Hannon <khannon@cocc.edu>
2020-05-12 05:20:03 -0700

    region_spec.rb 5 tests done

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,10 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
- #   describe "to_s" do
- #       it "returns valid name" do
-  #          let :name = 'FAKE'
- #           expect(name).to eq('FAKE')
- #       end
-  #  end
+    let(:region) {Region.new(name: 'FAKE') }
+
+    describe "attributes" do
+      it "has a name" do 
+        expect(region).to respond_to(:name)
+      end
+    end
+
+    describe "relationships" do
+      it "has many tickets" do
+        expect(region).to have_many(:tickets)
+      end
+    end
+
+    describe "validations" do
+      it "validates name" do
+        expect(region).to validate_presence_of(:name)
+      end
+      #it "validates length of name" do
+      #  expect(region).to validates_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+      #end
+      #it "validates uniqueness of name" do
+      #  expect(region).to validates_uniqueness_of(:name)
+      #end
+    end
+
+
+
 end
