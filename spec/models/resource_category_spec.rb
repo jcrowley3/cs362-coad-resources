a87858fab2de0fb22d21c703891190fe3e7b3c78
Kyle Hannon <khannon@cocc.edu>
2020-05-14 08:57:23 -0700

    Resource_category_spec added 4 tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+    let(:resource_category){ResourceCategory.new(name: 'FAKE')}
+
+    describe "relationships" do
+        it "has many tickets" do
+          expect(resource_category).to have_many(:tickets)
+        end
+        #it "has and belongs to many organizations" do
+        #    expect(resource_category).to  has_and_belongs_to_many(:organizations)
+        #end
+      end
+
+    describe "validations" do
+        it "validates presence of name" do
+            expect(resource_category).to validate_presence_of(:name)
+        end
+        it "validates length of name" do
+            expect(resource_category).to validate_length_of(:name)
+        end
+        it "validates uniqueness of name" do
+            expect(resource_category).to validate_uniqueness_of(:name).case_insensitive
+        end
+    end
 
 end
