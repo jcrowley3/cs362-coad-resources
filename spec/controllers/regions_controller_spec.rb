3d4ad94bd2d7daaacf3aa0c9756e6e3b728e8b43
Kyle Hannon <khannon@cocc.edu>
2020-05-18 09:11:06 -0700

    region_controller_spec almost done

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
+    context "unsigned user" do
+        let(:unsigned_in_user){create(:user)}
 
-end
+        describe "#index" do
+            specify {expect(get(:index)).to redirect_to(new_user_session_path)}
+        end
+        describe "#show" do
+            specify {expect(get(:show, params: {:id => 1})).to redirect_to(new_user_session_path)}
+        end
+        describe "#new" do
+            specify {expect(get(:new)).to redirect_to(new_user_session_path)}
+        end
+        describe "#create" do
+            specify {expect(get(:create, params: {:id => 1})).to redirect_to(new_user_session_path)}
+        end
+        describe "#edit" do
+            specify {expect(get(:edit, params: {:id => 1})).to redirect_to(new_user_session_path)}
+        end
+        describe "#update" do
+            specify {expect(get(:update, params: {:id => 1})).to redirect_to(new_user_session_path)}
+        end
+        describe "#destroy" do
+            specify {expect(get(:destroy, params: {:id => 1})).to redirect_to(new_user_session_path)}
+        end
+    end
+
+    context "as organization" do
+        let (:org_user) {create(:user, role: "organization")}
+
+        before(:each) do
+            org_user.confirm
+            sign_in(org_user)
+        end
+
+        describe "#index" do
+            specify {expect(get(:index)).to redirect_to(dashboard_path)}
+        end
+
+    end
+end 
