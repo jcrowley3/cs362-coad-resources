60d7929809b02b361c9eb88ed1929c79041b1f27
Crowley <jcrowley317@gmail.com>
2020-06-02 06:58:02 -0700

    feature tests

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe 'Deleting a Resource Category', type: :feature do
+RSpec.describe 'Deleting a Resource Category:', type: :feature do
+
+	let(:admin) {create(:user, :admin)}
+	let(:resource_category) {create(:resource_category)}
+
+	before :each do
+			log_in_as(admin)
+	end
+
+	context 'admin user' do
+		it "deletes a resurce category" do
+			visit new_resource_category_path
+			fill_in 'Name', with: 'Fake Category'
+			click_on "Add Resource Category"
+			expect(page).to have_content("Category successfully created")
+			click_on 'Fake Category'
+			click_on 'Delete'
+			#page.accept_alert
+			expect(page).to have_content("Associated tickets now belong")
+		end
+	end
 
 end
