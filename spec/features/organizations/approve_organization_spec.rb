417ff66469adb55fce03f9afc7f9bc7af676c624
Kyle Hannon <khannon@cocc.edu>
2020-06-01 19:21:46 -0700

    Finished organization features

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
 
+    let(:admin) { create(:user, :admin) }
+    let(:organization) {create(:organization, name: "Fake Organization")}
+
+    before :each do
+        log_in_as(admin)
+    end
+
+    context 'Approve organization application' do
+        it 'is approved' do
+            visit organization_path(organization)
+            #click_on "Fake Organization"
+            click_on 'Approve'
+            expect(page).to have_content('has been approved')
+        end
+    end
+
 end
--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
+    let(:organization) {create(:organization, :approved) }
+    let(:user) {create(:user, organization: organization)}
+    
+    before :each do
+        log_in_as(user)
+    end
 
+   context 'as an organization' do
+    it 'edits its organization information' do
+        visit edit_organization_path(organization)
+        fill_in 'Name', with: "New Name Fake Organization"
+        click_on 'Update Resource'
+        expect(page).to have_content("New Name Fake Organization")
+    end
+   end
 end
