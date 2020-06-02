2ce1f80a5585984164eb861ef3a29c82ad480929
Kyle Hannon <khannon@cocc.edu>
2020-06-01 21:16:53 -0700

    Added bad paths to users features

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -11,4 +11,25 @@ RSpec.describe 'Logging in', type: :feature do
 		find_by_id('commit').click
 		expect(current_path).to eq(dashboard_path)
 	end
+
+	it 'user logins with wrong email' do
+		visit root_path
+		click_on("Log in")
+		fill_in "Email", with: "Fake@email.com"
+		fill_in "Password", with: user.password
+		find_by_id('commit').click
+		expect(page).to have_content("Invalid Email or password")
+		expect(current_path).to eq('/users/sign_in')
+	end
+
+	it 'user logins with wrong password' do
+		visit root_path
+		click_on("Log in")
+		fill_in "Email", with: user.email
+		fill_in "Password", with: "Fake password"
+		find_by_id('commit').click
+		expect(page).to have_content("Invalid Email or password")
+		expect(current_path).to eq('/users/sign_in')
+
+	end
 end
